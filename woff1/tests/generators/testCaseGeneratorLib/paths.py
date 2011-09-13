877ca610d5ee0e7685eb0c0cfc50afc55b826702
Tal Leming <tal@typesupply.com>
2011-09-13 15:14:46 +0000

    Generate tests into the xhtml1 sub-directory.

--- woff1/tests/generators/testCaseGeneratorLib/paths.py
+++ woff1/tests/generators/testCaseGeneratorLib/paths.py
@@ -31,16 +31,16 @@ sfntTTFSourcePath = os.path.join(resourcesDirectory, "SFNT-TTF.ttf")
 
 # directories for test output
 userAgentDirectory = os.path.join(mainDirectory, "UserAgent")
-userAgentTestDirectory = os.path.join(userAgentDirectory, "Tests")
+userAgentTestDirectory = os.path.join(userAgentDirectory, "Tests", "xhtml1")
 userAgentTestResourcesDirectory = os.path.join(userAgentTestDirectory, "resources")
 userAgentFontsToInstallDirectory = os.path.join(userAgentDirectory, "FontsToInstall")
 
 formatDirectory = os.path.join(mainDirectory, "Format")
-formatTestDirectory = os.path.join(formatDirectory, "Tests")
+formatTestDirectory = os.path.join(formatDirectory, "Tests", "xhtml1")
 formatResourcesDirectory = os.path.join(formatTestDirectory, "resources")
 
 authoringToolDirectory = os.path.join(mainDirectory, "AuthoringTool")
-authoringToolTestDirectory = os.path.join(authoringToolDirectory, "Tests")
+authoringToolTestDirectory = os.path.join(authoringToolDirectory, "Tests", "xhtml1")
 authoringToolResourcesDirectory = os.path.join(authoringToolTestDirectory, "resources")
 
 if __name__ == "__main__":
